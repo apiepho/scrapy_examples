0d98dcc478dcf766c06fb38f3b00979daf4dadaa
piephoa <piephoa@c02pt1esg8wp.ftc.avagotech.net>
2017-01-24 14:10:34 -0700

    starting simple scrapy project and spider to crawl craigslist cars

---
+++ cl_test1/cl_test1/spiders/cars.py
@@ -0,0 +1,25 @@
+from scrapy.spiders  import Spider
+from scrapy.selector import Selector
+from cl_test1.items  import CraigsListItem
+
+class cars(Spider):
+    name = "cars"
+    allowed_domains = ["fortcollins.craigslist.org"]
+    start_urls = [
+    "https://fortcollins.craigslist.org/search/cto"
+    ]
+
+    def parse(self,response):
+         sel = Selector(response)
+         container_lists = sel.xpath('//li[@class="result-row"]')
+         items = []
+         for li in container_lists:
+             print "BBBB"
+             print li
+             item = CraigsListItem()
+             #item['ad_title']       = li.xpath('div[@class="title"]/a/text()').extract()
+             #item['ad_location']    = li.xpath('div[@class="category-location"]/span/text()').extract()
+             #item['ad_time']        = li.xpath('div[@class="info"]/div[@class="creation-date"]/span/text()').extract()
+             item['ad_description'] = li.xpath('p/a/text()').extract()
+             items.append(item)
+         return items
